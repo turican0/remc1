baddfa03b8ebc1be02da4ef09a90f9f3b6293eb6
Tomas Vesely <turican0@gmail.com>
2022-09-13 15:30:15 +0200

    update

---
+++ REMC1.cpp
@@ -0,0 +1,20 @@
+﻿// REMC1.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Spuštění programu: Ctrl+F5 nebo nabídka Ladit > Spustit bez ladění
+// Ladění programu: F5 nebo nabídka Ladit > Spustit ladění
+
+// Tipy pro zahájení práce:
+//   1. K přidání nebo správě souborů použijte okno Průzkumník řešení.
+//   2. Pro připojení ke správě zdrojového kódu použijte okno Team Explorer.
+//   3. K zobrazení výstupu sestavení a dalších zpráv použijte okno Výstup.
+//   4. K zobrazení chyb použijte okno Seznam chyb.
+//   5. Pokud chcete vytvořit nové soubory kódu, přejděte na Projekt > Přidat novou položku. Pokud chcete přidat do projektu existující soubory kódu, přejděte na Projekt > Přidat existující položku.
+//   6. Pokud budete chtít v budoucnu znovu otevřít tento projekt, přejděte na Soubor > Otevřít > Projekt a vyberte příslušný soubor .sln.
