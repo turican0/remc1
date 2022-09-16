fda97b18650befb32416280d7c430a07ed4a5c1c
Tomas Vesely <turican0@gmail.com>
2022-09-16 15:29:35 +0200

    mod structure

--- sub_main.h
+++ sub_main.h
@@ -14,4 +14,12 @@ extern __int16 word_A9990;
 extern int dword_A99A0;
 extern int dword_AE450;
 
+typedef struct {
+	int var0;
+} Type_str_AE400_AE3F0;
+
+extern Type_str_AE400_AE3F0 str_AE400_AE3F0;
+
+int dword_AE400_AE3F0();
+
 int sub_main(int argc, const char** argv, const char** envp);
