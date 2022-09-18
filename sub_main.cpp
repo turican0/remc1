7bafbf2474aab9bcd11b3080702ade000f509102
turican0 <41465132+turican0@users.noreply.github.com>
2022-09-18 19:35:01 +0200

    rename InitAnojoy_3F820_sub_3FB60

--- sub_main.cpp
+++ sub_main.cpp
@@ -662,7 +662,7 @@ void sub_3F54D(__int16 a1, __int16 a2, __int16 a3, __int16 a4);
 // void sub_3F587(__int16 a1, __int16 a2, __int16 a3, __int16 a4);
 unsigned __int8 sub_3F76D_sub_3FAAD(int a1);
 void InitDigijoy_3F7D0_3FB10(char* textBuffer);
-void sub_3F820_sub_3FB60(__int16 a2, int a3, int a4, int a5, char a6);
+void InitAnojoy_3F820_sub_3FB60(__int16 a2, int a3, int a4, int a5, char a6);
 void sub_3FB30_3FE70(__int16 a1, __int16 a2, char* a3);
 char sub_3FCA0_3FFE0(char a5, __int16 a6, __int16 a7, __int16 a8, __int16 a9, __int16 a10, __int16 a11);
 __int16 sub_40440_40780(char* textBuffer);
@@ -12237,7 +12237,7 @@ void sub_10000() {}; // weak
 //char sub_35F30_362F0(int a1, char a2, char a3, int a4, int a5, unsigned __int8 a6) { return 0; };
 int sub_38D09() { return 0; };// weak
 //unsigned __int8 sub_3F76D_sub_3FAAD(int a1) { return 0; };
-//char sub_3F820_sub_3FB60(__int16 a1, __int16 a2, int a3, int a4, int a5, char a6) { return 0; };
+//char InitAnojoy_3F820_sub_3FB60(__int16 a1, __int16 a2, int a3, int a4, int a5, char a6) { return 0; };
 //char sub_3FCA0_3FFE0(int a1, __int16 a2, __int16 a3, __int16 a4, char a5, __int16 a6, __int16 a7, __int16 a8, __int16 a9, __int16 a10, __int16 a11) { return 0; };
 //__int16 sub_41DD0_sub_42110(__int16 result, int a2) { return 0; };
 //char sub_43B20_sub_43E60(int a1, unsigned int a2) { return 0; };
@@ -37714,7 +37714,7 @@ int ProcessCommandLine_34DD0_35190(int argc, char** argv)//205DD0_
 		}
 		if (varAnojoy)
 		{
-			sub_3F820_sub_3FB60(firstChar, textBuffer4[0], textBuffer4[1], textBuffer4[2], textBuffer4[3]);
+			InitAnojoy_3F820_sub_3FB60(firstChar, textBuffer4[0], textBuffer4[1], textBuffer4[2], textBuffer4[3]);
 			if (byte_B7700_B76F0)
 			{
 				byte_B7700_B76F0 = 1;
@@ -37723,7 +37723,7 @@ int ProcessCommandLine_34DD0_35190(int argc, char** argv)//205DD0_
 		}
 		if (neverRun)
 		{
-			sub_3F820_sub_3FB60(firstChar, textBuffer4[0], textBuffer4[1], textBuffer4[2], textBuffer4[3]);
+			InitAnojoy_3F820_sub_3FB60(firstChar, textBuffer4[0], textBuffer4[1], textBuffer4[2], textBuffer4[3]);
 			if (byte_B7700_B76F0)
 			{
 				byte_B7700_B76F0 = 1;
@@ -45401,7 +45401,7 @@ void InitDigijoy_3F7D0_3FB10(char* textBuffer)
 }
 
 //----- (0003F820) --------------------------------------------------------
-void sub_3F820_sub_3FB60(__int16 a2, int a3, int a4, int a5, char a6)
+void InitAnojoy_3F820_sub_3FB60(__int16 a2, int a3, int a4, int a5, char a6)
 {
   char result; // al
   __int16 v8; // bx
@@ -57473,7 +57473,7 @@ void sub_51DF0(char a1)
   {
     if ( (a1 & 1) == 0 && (a1 & 0x20) == 0 )
       goto LABEL_8;
-    sub_3F820_sub_3FB60(v1, dword_12CAF8 + 60, dword_12CAF8 + 30, dword_12CAF8 + 36, a1);
+    InitAnojoy_3F820_sub_3FB60(v1, dword_12CAF8 + 60, dword_12CAF8 + 30, dword_12CAF8 + 36, a1);
     if ( !dword_9AD98 )
       goto LABEL_8;
   }
