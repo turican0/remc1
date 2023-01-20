23d059fe1262f2519be259492f303348aaf179c0
Tomas Vesely <turican0@gmail.com>
2023-01-20 15:29:04 +0100

    update 230120

--- sub_main.cpp
+++ sub_main.cpp
@@ -73904,6 +73904,42 @@ typedef struct {
 short crctab_ready = false;
 unsigned short crctab[256];
 
+unsigned long blong(unsigned char* p)
+{
+    unsigned long n;
+    n = p[0];
+    n = (n << 8) + p[1];
+    n = (n << 8) + p[2];
+    n = (n << 8) + p[3];
+    return n;
+}
+
+unsigned long llong(unsigned char* p)
+{
+    unsigned long n;
+    n = p[3];
+    n = (n << 8) + p[2];
+    n = (n << 8) + p[1];
+    n = (n << 8) + p[0];
+    return n;
+}
+
+unsigned long bword(unsigned char* p)
+{
+    unsigned long n;
+    n = p[0];
+    n = (n << 8) + p[1];
+    return n;
+}
+
+unsigned long lword(unsigned char* p)
+{
+    unsigned long n;
+    n = p[1];
+    n = (n << 8) + p[0];
+    return n;
+}
+
 long rnc_crc(void* data, unsigned long len)
 {
     unsigned short val;
@@ -73940,7 +73976,7 @@ long rnc_crc(void* data, unsigned long len)
 static void bitread_init(bit_stream* bs, unsigned char** p, unsigned char* pend)
 {
     if (pend - (*p) >= 0)
-        bs->bitbuf = *(uint16*)(*p);
+        bs->bitbuf = lword(*p);
     else
         bs->bitbuf = 0;
     bs->bitcount = 16;
@@ -73954,7 +73990,7 @@ static void bit_advance(bit_stream* bs, int n, unsigned char** p, unsigned char*
     {
         (*p) += 2;
         if (pend - (*p) >= 0)
-            bs->bitbuf |= (*(uint16*)(*p) << bs->bitcount);
+            bs->bitbuf |= (lword(*p) << bs->bitcount);
         bs->bitcount += 16;
     }
 }
@@ -74056,7 +74092,7 @@ static void bitread_fix(bit_stream* bs, unsigned char** p, unsigned char* pend)
     bs->bitcount -= 16;
     bs->bitbuf &= (1 << bs->bitcount) - 1; // remove the top 16 bits
     if (pend - (*p) >= 0)
-        bs->bitbuf |= (*(uint16*)(*p) << bs->bitcount);// replace with what's at *p
+        bs->bitbuf |= (lword(*p) << bs->bitcount);// replace with what's at *p
     bs->bitcount += 16;
 }
 
@@ -74077,10 +74113,10 @@ long rnc_unpack(void* packed, void* unpacked, unsigned int flags
 #ifdef COMPRESSOR
     long lee = 0;
 #endif
-    if (*(uint32*)(input) != RNC_SIGNATURE)
+    if (blong(input) != RNC_SIGNATURE)
         if (!(flags & RNC_IGNORE_HEADER_VAL_ERROR)) return RNC_HEADER_VAL_ERROR;
-    ret_len = *(uint32*)(input + 4);
-    inp_len = *(uint32*)(input + 8);
+    ret_len = blong(input + 4);
+    inp_len = blong(input + 8);
     if ((ret_len > (1 << 30)) || (inp_len > (1 << 30)))
         return RNC_HEADER_VAL_ERROR;
 
@@ -74092,9 +74128,9 @@ long rnc_unpack(void* packed, void* unpacked, unsigned int flags
     // Check the packed-data CRC. Also save the unpacked-data CRC
     // for later.
 
-    if (rnc_crc(input, inputend - input) != (long)*(uint16*)(input - 4))
+    if (rnc_crc(input, inputend - input) != (long)bword(input - 4))
         if (!(flags & RNC_IGNORE_PACKED_CRC_ERROR)) return RNC_PACKED_CRC_ERROR;
-    out_crc = *(uint16*)(input - 6);
+    out_crc = bword(input - 6);
 
     bitread_init(&bs, &input, inputend);
     bit_advance(&bs, 2, &input, inputend);      // discard first two bits
@@ -74229,11 +74265,11 @@ long rnc_unpack(void* packed, void* unpacked, unsigned int flags
 long UnpackM1(unsigned char* buffer, ulong bufsize)
 {
     //If file isn't compressed - return zero
-    if (*(uint32*)(buffer + 0) != RNC_SIGNATURE)
+    if (blong(buffer + 0) != RNC_SIGNATURE)
         return 0;
     // Originally this function was able do decompress data without additional buffer.
     // If you know how to decompress the data this way, please correct this.
-    ulong packedsize = *(uint32*)(buffer + 4);
+    ulong packedsize = blong(buffer + 4);
     if (packedsize > bufsize)
         packedsize = bufsize;
     void* packed = malloc(packedsize);
@@ -74244,10 +74280,9 @@ long UnpackM1(unsigned char* buffer, ulong bufsize)
     return retcode;
 }
 
-int RncUnpack_62B60_63070(uint8_t* input, uint8_t* output) {
-    int result = UnpackM1(input, 0);
+int RncUnpack_62B60_63070_new(uint8_t* input, uint8_t* output) {
+    int result = UnpackM1(input, 100000000000);
     return result;
-
 };
 //-----------------
 //-----------------
@@ -74255,7 +74290,7 @@ int RncUnpack_62B60_63070(uint8_t* input, uint8_t* output) {
 int counter_62B60_63070 = 0;
 //SYNCHRONIZED WITH REMC1
 //int sub_62B60_63070_new(uint8_t* input, uint8_t* output)
-int RncUnpack_62B60_63070_orig(uint8_t* input, uint8_t* output)//233B60_
+int RncUnpack_62B60_63070(uint8_t* input, uint8_t* output)//233B60_
 {
     unsigned int i; // ecx
     //int* data; // esi
@@ -74358,14 +74393,19 @@ int RncUnpack_62B60_63070_orig(uint8_t* input, uint8_t* output)//233B60_
         {
             //debug
             //233C6F
-            if (counter_62B60_63070 == 0x2)
+            if(input[0x1fe2 + 15]==0xff)
+            {
+                counter_62B60_63070++;
+                counter_62B60_63070--;
+            }
+            if (counter_62B60_63070 == 0x82b)
             {
                 counter_62B60_63070++;
                 counter_62B60_63070--;
             }
             uint8 origbyte20;
             uint8 remakebyte20;
-            int comp20 = compare_with_sequence("00233C6F-FFFFFF02", (uint8_t*)v15, 0x233C6F, counter_62B60_63070, 0x10, 0x10, &origbyte20, &remakebyte20, 0, 0);            
+            //int comp20 = compare_with_sequence("00233C6F-FFFFFF02", (uint8_t*)v15, 0x233C6F, counter_62B60_63070, 0x10, 0x10, &origbyte20, &remakebyte20, 0, 0);            
 
             counter_62B60_63070++;
             //debug
@@ -74389,7 +74429,8 @@ int RncUnpack_62B60_63070_orig(uint8_t* input, uint8_t* output)//233B60_
             if (!--rncChCount_9B14A)
                 break;
             i=HufRead_62CFD_6320D(&CX_, rncDist_9B040, &v6x);
-            v22 = i;
+            //v22 = i;
+            v22 = CX_;
             i = HufRead_62CFD_6320D(&CX_, rncLen_9B0C0, &v6x);
             i = CX_;
             LOWORD(i) = i + 2;
@@ -74626,7 +74667,7 @@ __int16 BitRead_62D40_63250(uint16* CX_, uint8 a1, uint8** a2x)//233D40_
 
   //debug
 #ifdef debug1
-  if (compare_index_62D40_63250 == 0x2616)
+  if (compare_index_62D40_63250 == 0x2971)
   {
       compare_index_62D40_63250++;
       compare_index_62D40_63250--;
@@ -74639,7 +74680,80 @@ __int16 BitRead_62D40_63250(uint16* CX_, uint8 a1, uint8** a2x)//233D40_
   comp20 = compare_with_sequence("00233D40-0025914C", (uint8_t*)&word_9B14C, 0x233D40, compare_index_62D40_63250, 0x2, 0x2, &origbyte20, &remakebyte20, 0, 0);
   comp20 = compare_with_sequence("00233D40-FFFFFFF4", (uint8_t*)&a1, 0x233D40, compare_index_62D40_63250, 0x1, 0x1, &origbyte20, &remakebyte20, 0, 0);
 
-  comp20 = compare_with_sequence("00233D42-FFFFFF01", (uint8_t*)*a2x, 0x233D40, compare_index_62D40_63250, 0x10, 0x10, &origbyte20, &remakebyte20, 0, 0);
+  char tempa2x[0x10];
+  for (int iii = 0; iii < 0x10; iii++)
+  {
+      tempa2x[iii] = (*a2x)[iii];
+  }
+  if ((compare_index_62D40_63250 >= 0x20a8)&&(compare_index_62D40_63250 <= 0x20ab))
+      tempa2x[15] = 0;
+  if ((compare_index_62D40_63250 >= 0x20ac) && (compare_index_62D40_63250 <= 0x20af))
+  {
+      tempa2x[13] = 0;
+      tempa2x[14] = 0;
+      tempa2x[15] = 0;
+  }
+  if ((compare_index_62D40_63250 >= 0x20b0) && (compare_index_62D40_63250 <= 0x20b1))
+  {
+      tempa2x[11] = 0;
+      tempa2x[12] = 0;
+      tempa2x[13] = 0;
+      tempa2x[14] = 0;
+      tempa2x[15] = 0;
+  }
+  if ((compare_index_62D40_63250 >= 0x20b2) && (compare_index_62D40_63250 <= 0x20b5))
+  {
+      tempa2x[9] = 0;
+      tempa2x[10] = 0;
+      tempa2x[11] = 0;
+      tempa2x[12] = 0;
+      tempa2x[13] = 0;
+      tempa2x[14] = 0;
+      tempa2x[15] = 0;
+  }
+  if ((compare_index_62D40_63250 >= 0x20b6) && (compare_index_62D40_63250 <= 0x20b9))
+  {
+      tempa2x[7] = 0;
+      tempa2x[8] = 0;
+      tempa2x[9] = 0;
+      tempa2x[10] = 0;
+      tempa2x[11] = 0;
+      tempa2x[12] = 0;
+      tempa2x[13] = 0;
+      tempa2x[14] = 0;
+      tempa2x[15] = 0;
+  }
+  if ((compare_index_62D40_63250 >= 0x20ba) && (compare_index_62D40_63250 <= 0x20bf))
+  {
+      tempa2x[5] = 0;
+      tempa2x[6] = 0;
+      tempa2x[7] = 0;
+      tempa2x[8] = 0;
+      tempa2x[9] = 0;
+      tempa2x[10] = 0;
+      tempa2x[11] = 0;
+      tempa2x[12] = 0;
+      tempa2x[13] = 0;
+      tempa2x[14] = 0;
+      tempa2x[15] = 0;
+  }
+  if ((compare_index_62D40_63250 >= 0x20c0) && (compare_index_62D40_63250 <= 0x20c1))
+  {
+      tempa2x[3] = 0;
+      tempa2x[4] = 0;
+      tempa2x[5] = 0;
+      tempa2x[6] = 0;
+      tempa2x[7] = 0;
+      tempa2x[8] = 0;
+      tempa2x[9] = 0;
+      tempa2x[10] = 0;
+      tempa2x[11] = 0;
+      tempa2x[12] = 0;
+      tempa2x[13] = 0;
+      tempa2x[14] = 0;
+      tempa2x[15] = 0;
+  }
+  //comp20 = compare_with_sequence("00233D42-FFFFFF01", (uint8_t*)tempa2x, 0x233D40, compare_index_62D40_63250, 0x10, 0x10, &origbyte20, &remakebyte20, 0, 0);
   comp20 = compare_with_sequence("00233D42-FFFFFF03", (uint8_t*)CX_, 0x233D40, compare_index_62D40_63250, 0x2, 0x2, &origbyte20, &remakebyte20, 0, 0);
 
   comp20 = compare_with_sequence("00233D42-00258FC0", (uint8_t*)rncRaw_9AFC0, 0x233D40, compare_index_62D40_63250, 0x10, 0x10, &origbyte20, &remakebyte20, 0, 0);
@@ -74993,7 +75107,7 @@ int counter_634E0_639F0 = 0;
 int sub_634E0_639F0(Pathstruct* pathstruct)//2344e0_
 {
     //debug
-    if (counter_634E0_639F0 == 0x1)
+    if (counter_634E0_639F0 == 0x5)
     {
         counter_634E0_639F0++;
         counter_634E0_639F0--;
@@ -75019,7 +75133,7 @@ int sub_634E0_639F0(Pathstruct* pathstruct)//2344e0_
 		pathstruct->var36_size_buffer = GetRNCFilesize_63910_63E20(pathstruct);
 		if (pathstruct->var36_size_buffer <= 0)
 			return 0;
-		*pathstruct->colorPalette_var28 = (uint8_t*)mallocVar(pathstruct->var36_size_buffer);
+		*pathstruct->colorPalette_var28 = (uint8_t*)mallocVar(pathstruct->var36_size_buffer+1);
 		if (!(*pathstruct->colorPalette_var28))
 			return -1;
         
